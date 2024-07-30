1f644f2fdcb1439aba562bb35d9976388f62700a
Dipak Makwana <dipakmeck@gmail.com>
2024-07-30 22:58:26 +0530

    embed video with react-player

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Weekday from '../Weekday';
 import Weekend from '../Weekend';
 import dipak from '../../assets/img/dipak-makwana.png';
+import ReactPlayer from 'react-player';
 
 export default function Home() {
 	const day = new Date().getDay();
+	const video = 'https://www.youtube.com/watch?v=tjuUidyCQ3M';
 	return (
 		<>
 			<h1>Home</h1>
 			{day >= 1 && day <= 5 ? <Weekday /> : <Weekend />}
 			<img src={dipak} alt='' />
+			<ReactPlayer url={video} playing={false} volume={0.5} />
 		</>
 	);
 }
