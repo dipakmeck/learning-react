91a94f8e862a7b374eae00d3778419d65c99a992
Dipak Makwana <dipakmeck@gmail.com>
2024-07-30 22:39:07 +0530

    import image

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Weekday from '../Weekday';
 import Weekend from '../Weekend';
+import dipak from '../../assets/img/dipak-makwana.png';
 
 export default function Home() {
 	const day = new Date().getDay();
@@ -8,6 +9,7 @@ export default function Home() {
 		<>
 			<h1>Home</h1>
 			{day >= 1 && day <= 5 ? <Weekday /> : <Weekend />}
+			<img src={dipak} alt='' />
 		</>
 	);
 }
